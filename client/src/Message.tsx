56350b1fb0c6bb25ff9dd341aae92ca31b09ef17
Rishi Sekhar <rishi.sek@gmail.com>
2022-05-21 12:51:46 -0400

    Implement removing and undoing

--- client/src/Message.tsx
+++ client/src/Message.tsx
@@ -3,23 +3,23 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Select from "components/Select";
 import Input from "components/Input";
-import { IMessage } from "@server/models/message";
+import { IMessage, IField } from "@server/models/message";
 
-const Wrapper = styled.div`
-  
-`;
+const Wrapper = styled.div``;
 
 const ProtobufField = styled.div``;
 
-interface IProtobufField {
-  name: string;
-  type: string;
-  number: number;
+interface Edit {
+  type: "type" | "name" | "number" | "remove";
+  index: number;
+  old_value?: string;
+  old_field?: IField;
 }
 
 function Message() {
-  let [fields, setFields] = useState<Array<IProtobufField>>([
-    { type: "string" } as IProtobufField,
+  let [edits, setEdits] = useState<Array<Edit>>([]);
+  let [fields, setFields] = useState<Array<IField>>([
+    { type: "string" } as IField,
   ]);
   let [name] = useState<string>("Test");
   let [isNew, setIsNew] = useState<boolean>(false);
@@ -34,10 +34,14 @@ function Message() {
       .catch((err) => setIsNew(true));
   }, []);
 
-  const addField: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    setFields((oldArray) => [
-      ...oldArray,
-      { type: "string" } as IProtobufField,
+  const addField = (
+    index = fields.length,
+    field = { type: "string" } as IField
+  ) => {
+    setFields((fields) => [
+      ...fields.slice(0, index),
+      field,
+      ...fields.slice(index),
     ]);
   };
 
@@ -60,6 +64,7 @@ function Message() {
     ): React.FormEventHandler<HTMLElement> =>
     (e) => {
       let field = fields[index];
+      let old_value = field[prop];
       switch (prop) {
         case "type":
           field[prop] = (e.target as HTMLSelectElement).value;
@@ -71,8 +76,49 @@ function Message() {
           field[prop] = parseInt((e.target as HTMLInputElement).value);
           break;
       }
-      setFields([...fields.slice(0, index), field, ...fields.slice(index + 1)]);
+      setFields((fields) => [
+        ...fields.slice(0, index),
+        field,
+        ...fields.slice(index + 1),
+      ]);
+      setEdits((edits) => [
+        ...edits,
+        { type: prop, index: index, old_value: old_value.toString() },
+      ]);
     };
+
+  const remove = (index: number) => () => {
+    let old_value = fields[index];
+    setFields((fields) => [
+      ...fields.slice(0, index),
+      ...fields.slice(index + 1),
+    ]);
+    setEdits((edits) => [
+      ...edits,
+      { type: "remove", index: index, old_field: old_value },
+    ]);
+  };
+
+  const undo = () => {
+    let edit = edits[edits.length - 1];
+    switch (edit.type) {
+      case "remove":
+        addField(edit.index, edit.old_field);
+        break;
+      default:
+        let field = fields[edit.index];
+        if (!edit.old_value) break;
+        if (edit.type === "number") field[edit.type] = parseInt(edit.old_value);
+        else field[edit.type] = edit.old_value;
+
+        setFields((fields) => [
+          ...fields.slice(0, edit.index),
+          field,
+          ...fields.slice(edit.index + 1),
+        ]);
+    }
+    setEdits((edits) => edits.slice(0, edits.length - 1));
+  };
 
   return (
     <Wrapper>
@@ -99,10 +145,12 @@ function Message() {
             value={field.number}
             onChange={onFieldChange(index, "number")}
           />
+          <button onClick={remove(index)}>-</button>
         </ProtobufField>
       ))}
-      <button onClick={addField}>+</button>
-      <button onClick={submit}>+</button>
+      <button onClick={() => addField()}>+</button>
+      <button onClick={submit}>Save</button>
+      <button onClick={undo}>Undo</button>
     </Wrapper>
   );
 }
